bb0474873b9c942f38e67b8796db7c863389f519
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-01-25 14:00:11 -0600

    Add line to script to launch CS50 my music notes project on startup

--- bin/.local/bin/ubuntu_server_startup.sh
+++ bin/.local/bin/ubuntu_server_startup.sh
@@ -3,4 +3,5 @@
 # commands to run on server startup
 (sudo ufw reload) &&
 (sudo qbittorrent-nox &) &&
-(sudo node /home/caleb/foundryvtt/resources/app/main.js --dataPath=$HOME/foundrydata &)
+(sudo node /home/caleb/foundryvtt/resources/app/main.js --dataPath=$HOME/foundrydata &) &&
+(python3 -m flask run --host=0.0.0.0 --port=7076)
